3931a17985e0a5ad6c6837a97000e54777c0a333
Edward Simpson <76537760+esimpson07@users.noreply.github.com>
2022-11-19 11:06:49 -0500

    Update servoplanner.ino
    Getting ready to change the code very substantially

--- servoplanner.ino
+++ servoplanner.ino
@@ -5,14 +5,15 @@ Servo s3;
 
 /*
  * The length between the rotation top piece * bottom servo center is 50 mm
- * The length between the foot and the middle of the bottom servo is 108 mm
+ * The length between the center of the ball and the middle of the bottom servo is 100 mm
  */
 
 double l1 = 50;
 double l2 = 100;
-double h;
-int deltaTime = 4;
-double angle;
+double h = 80; //80 is the highest the leg can go safely; 0 would be inside the servo; 150 is max theoretical extension
+int deltaTime = 5;
+double fwdTanAngle;
+double crabTanAngle;
 double angle1;
 double angle2;
 double angle3;
@@ -33,7 +34,7 @@ void findAngles(double height, double pos) {
   angle3 = (180 / PI) * acos((squareValue(l1) + squareValue(l2) - squareValue(l3)) / (2 * l1 * l2));
   angle2 = (180 / PI) * acos((squareValue(l1) + squareValue(l3) - squareValue(l2)) / (2 * l1 * l3));
   angle1 = (180 / PI) * acos((squareValue(l3) + squareValue(l2) - squareValue(l1)) / (2 * l3 * l2));
-  angle = (180 / PI) * (atan(pos / height));
+  fwdTanAngle = (180 / PI) * (atan(pos / height));
   Serial.print("3: ");
   Serial.println(angle3);  
   Serial.print("2: ");
@@ -41,14 +42,27 @@ void findAngles(double height, double pos) {
   Serial.print("1: ");
   Serial.println(angle1);
   Serial.print("angle2 - angle: ");
-  Serial.println(angle2 - angle);
+  Serial.println(angle2 - fwdTanAngle);
   /*Serial.println(angle1);
   Serial.println(angle2);
   Serial.println(angle3);*/
 }
 
 void loop() {
-  h = 95;
+  for(int i = -60; i <= 60; i ++) {
+    double extAngle = (180 / PI) * atan(i / h);
+    double j = sqrt((i * i) + (h * h));
+    findAngles(j,60);
+    s1.write(90 + angle1);
+    s2.write(90 - (angle2 - angle));
+    s3.write(angle3);
+    delay(deltaTime);
+  }
+  delay(250);
+}
+
+
+  /*h = 95;
   for(int i = 100; i >= 0; i --) {
     findAngles(h,i);
     s1.write(90);
@@ -63,5 +77,4 @@ void loop() {
     s2.write(90 - (angle2 - angle));
     s3.write(angle3);
     delay(deltaTime);
-  }
-}
+  }*/
